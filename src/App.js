da93be7af0fb6c2c344168600597d7ec11007678
vu.van.phong <phong95663@gmail.com>
2020-05-05 19:48:16 +0700

    Change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React he
+          Learn React change
         </a>
       </header>
     </div>
